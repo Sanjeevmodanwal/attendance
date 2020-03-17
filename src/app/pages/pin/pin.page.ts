7bebc3c1103efd512b42d87a6a70687d24e78842
Sanjeevmodanwal <sanjiv28890@gmail.com>
2020-03-17 21:17:07 +0530

    changes

--- src/app/pages/pin/pin.page.ts
+++ src/app/pages/pin/pin.page.ts
@@ -90,7 +90,7 @@ export class PinPage implements OnInit {
   ok(){
 	  this.presentLoading();
     let httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}), };
-    this.http.post('http://localhost/api/api.php?tag=checking',{"data":this.items},httpOptions).subscribe( (data:any) => {
+    this.http.post('http://essglobal.online/ess_crm/response_andr.php?tag=checking',{"data":this.items},httpOptions).subscribe( (data:any) => {
           console.log(data[0]['checkRslt']);
 		  
 		if(data[0]['checkRslt']==1){
@@ -102,13 +102,46 @@ export class PinPage implements OnInit {
 			this.two = "";
 			this.three = "";
 			this.four = "";
-		}else{
+		}
+		
+		if(data[0]['checkRslt']==2){
 			this.loadingController.dismiss();
-			let msg='danger';
+			let msg="You have entered invalid pin.";
+			let color="warning";
+			this.presentToast(msg,color);
+		}
+		if(data[0]['checkRslt']==3){
+			this.loadingController.dismiss();
+			let msg="Successfully Logout";
+			let color="success";
+			this.presentToast(msg,color);
+			this.one = "";
+			this.two = "";
+			this.three = "";
+			this.four = "";
+		}
+		if(data[0]['checkRslt']==4){
+			this.loadingController.dismiss();
+			let msg="Successfully Logout";
+			let color="success";
+			this.presentToast(msg,color);
+			this.one = "";
+			this.two = "";
+			this.three = "";
+			this.four = "";
+		}
+		if(data[0]['checkRslt']==5){
+			this.loadingController.dismiss();
+			let msg='You are in wrong network zone';
 			let color="danger";
 			this.presentToast(msg,color);
 		}
-		  
+		if(data[0]['checkRslt']==6){
+			this.loadingController.dismiss();
+			let msg='Passcode must be of four digits';
+			let color="danger";
+			this.presentToast(msg,color);
+		} 
 		  
     });
   }
